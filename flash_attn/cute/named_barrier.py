8b020a048af4f9d6fe57b094cc99daf7b9165025
drisspg <drisspguessous@gmail.com>
2026-07-24 22:16:52 -0700

    Add guarded SM100 smaller-tile kernels

--- flash_attn/cute/named_barrier.py
+++ flash_attn/cute/named_barrier.py
@@ -23,6 +23,7 @@ class NamedBarrierFwdSm100(enum.IntEnum):
     SoftmaxStatsW5 = enum.auto()
     SoftmaxStatsW6 = enum.auto()
     SoftmaxStatsW7 = enum.auto()
+    SoftmaxSmemP = enum.auto()  # barrier across softmax warpgroup for SMEM-P visibility
 
 
 class NamedBarrierBwd(enum.IntEnum):
